6c093200aa8624f5b3e222a621567d1103ad1440
Varsha16005 <124782329+Varsha16005@users.noreply.github.com>
2025-06-30 12:39:12 +0530

    uploaded junit assertions task and result

---
+++ Week2/junitassertions/src/main/java/com/example/junitassertions/JunitassertionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.junitassertions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitassertionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitassertionsApplication.class, args);
+	}
+
+}
