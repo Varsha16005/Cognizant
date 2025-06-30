a380325f5e794a0de76793b4b5d4f5205b4963b3
Varsha16005 <124782329+Varsha16005@users.noreply.github.com>
2025-06-30 12:20:39 +0530

    uploaded the junit setup task along with the output screenshot

---
+++ Week2/junitsetup/src/main/java/com/example/junitsetup/JunitsetupApplication.java
@@ -0,0 +1,13 @@
+package com.example.junitsetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitsetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitsetupApplication.class, args);
+	}
+
+}
---
+++ Week2/junitsetup/src/test/java/com/example/junitsetup/JunitsetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junitsetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitsetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
